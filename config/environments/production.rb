1c23b2026806513e5312a5357dbab8310afe6ff4
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-09-30 07:32:02 -0400

    added username to smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
     :port                 => 587,
     :domain               => 'gmail.com',
     :enable_starttls_auto => true,
-    :user_name            => 'EMAIL_ADDRESS@gmail.com',
+    :user_name            => 'nbr625@gmail.com',
     :password             => 'pass',
     :authentication       => "plain"
 
