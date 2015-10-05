e7677f8aca11e98d37b5b1be0bb7cfd85e28d5a8
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-10-05 13:07:20 -0400

    modified rspec configuration and set up contact factory

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
-  factory :contact do
+  factory :contact do |f|
+  	f.name {Faker::Name.name}
+  	f.email {Faker::Internet.safe_email('Nicolas')}
+  	f.message {Faker::Lorem.sentences}
     
   end
 
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:contact) {build(:contact)}
+  it {should be_valid}
+
+  describe "invalid when using comma" do
+  	contact.email = 'user@someplace,com'
+  	it {should_not be_valid}
+  end
+  
+
 end
